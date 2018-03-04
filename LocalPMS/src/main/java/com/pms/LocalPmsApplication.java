98d6d077174723e92691de1a9d3464ab4214ad4b
saurav agarwala <saurav.agarwala17@gmail.com>
2018-03-04 18:43:33 +0530

    basic Structure

---
+++ LocalPMS/src/main/java/com/pms/LocalPmsApplication.java
@@ -0,0 +1,12 @@
+package com.pms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocalPmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocalPmsApplication.class, args);
+	}
+}
